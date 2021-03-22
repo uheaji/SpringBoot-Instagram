81efd84a3051970e57772451f6566bea5676519b
uheaji <yoolbk@naver.com>
2021-03-22 12:39:05 +0900

    2. 인스타그램 클론 코딩 - 시큐리티 설정 완료(OAuth2.0, CORS 설정은 추후 진행)

---
+++ src/main/java/com/cos/costagram/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.cos.costagram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// 모델: Image, User, Likes, Follow, Tag
+	// auth 주소 : 인증 필요없음.
+	// static 폴더
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/", "/user/**", "/image/**", "/follow/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest()
+			.permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/loginForm")
+			.loginProcessingUrl("/");
+			// OAuth2.0과 CORS는 나중에!!
+	}
+}
